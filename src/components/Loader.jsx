b3da295e2b7ccb2e7dd1509942674c95da594125
BlueApple865729 <teikonami@gmail.com>
2023-05-19 23:21:01 -0800

    loader top 0

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 export default function Loader(props) {
     return (
-        <div className="fixed z-[9999] w-full h-[100vh] flex flex-col items-center justify-center bg-[#ffffffbb]">
+        <div className="fixed z-[9999] w-full h-[100vh] flex flex-col items-center justify-center bg-[#ffffffbb] top-0">
             <div class="lds-spinner">
                 <div></div>
                 <div></div>
@@ -15,7 +15,7 @@ export default function Loader(props) {
                 <div></div>
                 <div></div>
             </div>
-            <div className="mt-[10px] text-[20px]">Loading</div>
+            <div className="mt-[10px] text-[20px] font-bold">Loading</div>
         </div>
     );
 }
