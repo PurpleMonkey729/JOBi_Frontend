fddf0701d934cd55d28356617ab2e1f075a751f7
BlueApple865729 <teikonami@gmail.com>
2023-05-22 23:10:58 -0800

    404 Page updated

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,13 +1,16 @@
 export default function BusinessInfoPage() {
     return (
-        <div className="absolute top-0 w-full h-[100vh] flex flex-col justify-center items-center">
-            <img src="/src/img/alert.png" alt="alert" />
-            <div className="flex items-center">
-                <div className="text-[200px] font-bold">4</div>
-                <div className="w-[100px] h-[150px] flex justify-center items-center text-[#f44336] text-[75px] font-bold rounded-[100%] border-[20px] border-[#555]">?</div>
-                <div className="text-[200px] font-bold">4</div>
-            </div>
-            <div className="text-[16px]">該当するページが見つかりません!</div>
+        <div className="p404 relative flex flex-col justify-center px-[20px] bg-[#d0e9ff] h-full min-h-[100vh]">
+            <img src="/src/img/logo.png" className="mx-auto mb-[25px]" />
+            <div className="w-[50%] h-[2px] bg-[#333] mx-auto db:w-full"></div>
+            <div className="mt-[-11px] w-[20px] h-[20px] bg-[#333] rounded-[20px] mx-auto border-[5px] border-[#d0e9ff]"></div>
+            <h2 className="text-center mb-[50px]">おっとっと！ ページが見つかりません！</h2>
+            <h1 className="text-center text-[220px] font-bold tracking-[-40px] ml-[-40px]">
+                <span>4</span>
+                <span>0</span>
+                <span>4</span>
+            </h1>
+            <p className="text-center mt-[75px] text-[20px] font-bold">申し訳ありませんが、リクエストされたページが見つかりませんでした。</p>
         </div>
     );
 }
